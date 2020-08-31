b8ac4757ba01b7a6e4da23f5c8548491fd8988dd
Anando <anandoghose@gmail.com>
2020-08-31 17:52:10 +0530

    getting ready

--- inventory/inventory/settings.py
+++ inventory/inventory/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'iq(xpx5$5y%2g$b0-+3cveszbv!o_51z8ohuyv2@oz)09n(6^9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rsinventory.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
