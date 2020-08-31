2f6bc213184eeb43b0cd0a5ed20466c48eea61ac
Anando <anandoghose@gmail.com>
2020-08-31 19:11:21 +0530

    postgre

--- inventory/settings.py
+++ inventory/settings.py
@@ -122,3 +122,7 @@ TIME_ZONE = 'Asia/Calcutta'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
+
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
