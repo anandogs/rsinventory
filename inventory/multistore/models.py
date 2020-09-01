041ecc05b37a5aad7488ba9a6ddcbf517c0bc40c
Anando <anandoghose@gmail.com>
2020-09-01 08:14:52 +0530

    Going back to pre-heroku days.. the good days.. sigh

---
+++ inventory/multistore/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.conf import settings
+from datetime import datetime
+
+
+
+class SKU(models.Model):
+    sku_code = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f'{self.sku_code}'
+
+class Location(models.Model):
+    location_name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f'{self.location_name}'
+
+class Stock(models.Model):
+
+    timestamp = models.DateTimeField(auto_now=True)
+    order_no = models.CharField(max_length=64)
+    sku = models.ForeignKey(SKU, on_delete=models.DO_NOTHING)
+    quantity = models.IntegerField()
+    location = models.ForeignKey(Location, on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return f'{self.pk}, {self.timestamp}, {self.order_no}, {self.sku}, {self.quantity}, {self.location}, {self.user}'
---
+++ inventory/multistore/upload.py
@@ -0,0 +1,14 @@
+import pandas as pd
+# import . models
+from . models import Stock, SKU, Location
+
+df = pd.read_csv('/Users/anandoghose/Desktop/inventory_export_1.csv', low_memory=False)
+
+df['SKU'].dropna(inplace=True)
+df['SKU'].drop_duplicates(inplace=True)
+
+for i in range(len(df)):
+    sku_code = df.loc[i,'SKU']
+    print(sku_code)
+    sku_list = SKU.objects.create(sku_code=sku_code)
+    sku_list.save()
